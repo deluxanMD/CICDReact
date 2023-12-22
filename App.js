1479367be703942477d9820e7df8146411c01f00
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-12-22 06:13:25 +0530

    add text input

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput value="Deluxan Mariathasan" />
       <Text>Welcome to the project</Text>
       <StatusBar style="auto" />
     </View>
